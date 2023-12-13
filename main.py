28ce1b9debcf9a38dbb1ff785cce0df787bfa2f4
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2023-12-13 11:33:17 -0600

    Initial main

--- main.py
+++ main.py
@@ -3,18 +3,16 @@
 from utils  import BANNER, Config, LOGGER
 
 try:
-    # INitiate logging with banner
+    # Initiate logging with banner
     LOGGER.info(BANNER)
 
     # Read in configuration files
     CONFIG = Config()
 
 except Exception as e:
-
     # Report any wildcard errors
     LOGGER.error(f"An error occurred: {e}")
 
 finally:
-
     # Report exit
     LOGGER.info("Exiting...")
